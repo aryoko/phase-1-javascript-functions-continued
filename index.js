690e19a21f98a506f8d9d9bd2ce2ebcc4dbf9692
Ary Okoli <aco.umeadi@gmail.com>
2023-03-20 20:39:47 -0400

    functions lab part 2 complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+mondayWork();
+
+const wrapAdjective = function (result = "*") {
+  const wrapper = "You are"
+  return function (emphatic = "special") {
+    return `${wrapper} ${result}${emphatic}${result}!`
+  }
+    
+}
+  
